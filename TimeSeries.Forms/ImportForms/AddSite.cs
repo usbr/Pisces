8dbcc6b4fde6e5ea4fe6d614e862bec3dec354c7
ktarbet <ktarbet@gmail.com>
2014-10-29 13:39:11 -0600

    basic design for adding sites

---
+++ TimeSeries.Forms/ImportForms/AddSite.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Reclamation.TimeSeries.Forms.ImportForms
+{
+    public partial class AddSite : Form
+    {
+        public AddSite()
+        {
+            InitializeComponent();
+        }
+    }
+}
