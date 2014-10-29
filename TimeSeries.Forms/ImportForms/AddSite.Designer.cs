a56a28b8624c5e98263a0106314b1d3c13fdf023
ktarbet <ktarbet@gmail.com>
2014-10-29 13:39:11 -0600

    basic design for adding sites

---
+++ TimeSeries.Forms/ImportForms/AddSite.Designer.cs
@@ -0,0 +1,275 @@
+﻿namespace Reclamation.TimeSeries.Forms.ImportForms
+{
+    partial class AddSite
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBoxSiteID = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBoxdescription = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBoxlatitude = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBoxlongitude = new System.Windows.Forms.TextBox();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBoxElevatoion = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.textBoxTimezone = new System.Windows.Forms.TextBox();
+            this.textBoxInstall = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // textBoxSiteID
+            // 
+            this.textBoxSiteID.Location = new System.Drawing.Point(86, 18);
+            this.textBoxSiteID.Name = "textBoxSiteID";
+            this.textBoxSiteID.Size = new System.Drawing.Size(99, 22);
+            this.textBoxSiteID.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(3, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(41, 17);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "siteid";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(3, 43);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(77, 17);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "description";
+            // 
+            // textBoxdescription
+            // 
+            this.textBoxdescription.Location = new System.Drawing.Point(86, 43);
+            this.textBoxdescription.Name = "textBoxdescription";
+            this.textBoxdescription.Size = new System.Drawing.Size(208, 22);
+            this.textBoxdescription.TabIndex = 2;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(26, 42);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(54, 17);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "latitude";
+            // 
+            // textBoxlatitude
+            // 
+            this.textBoxlatitude.Location = new System.Drawing.Point(101, 39);
+            this.textBoxlatitude.Name = "textBoxlatitude";
+            this.textBoxlatitude.Size = new System.Drawing.Size(166, 22);
+            this.textBoxlatitude.TabIndex = 4;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(26, 67);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(66, 17);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "longitude";
+            // 
+            // textBoxlongitude
+            // 
+            this.textBoxlongitude.Location = new System.Drawing.Point(101, 64);
+            this.textBoxlongitude.Name = "textBoxlongitude";
+            this.textBoxlongitude.Size = new System.Drawing.Size(166, 22);
+            this.textBoxlongitude.TabIndex = 6;
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tabControl1.Location = new System.Drawing.Point(0, 0);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(674, 542);
+            this.tabControl1.TabIndex = 8;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.dataGridView1);
+            this.tabPage1.Controls.Add(this.panel1);
+            this.tabPage1.Location = new System.Drawing.Point(4, 25);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(666, 513);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "new site";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.textBoxInstall);
+            this.tabPage2.Controls.Add(this.label7);
+            this.tabPage2.Controls.Add(this.textBoxTimezone);
+            this.tabPage2.Controls.Add(this.label6);
+            this.tabPage2.Controls.Add(this.label5);
+            this.tabPage2.Controls.Add(this.textBoxElevatoion);
+            this.tabPage2.Controls.Add(this.label3);
+            this.tabPage2.Controls.Add(this.label4);
+            this.tabPage2.Controls.Add(this.textBoxlatitude);
+            this.tabPage2.Controls.Add(this.textBoxlongitude);
+            this.tabPage2.Location = new System.Drawing.Point(4, 25);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(769, 632);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "advanced";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(26, 94);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(65, 17);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "elevation";
+            // 
+            // textBoxElevatoion
+            // 
+            this.textBoxElevatoion.Location = new System.Drawing.Point(101, 91);
+            this.textBoxElevatoion.Name = "textBoxElevatoion";
+            this.textBoxElevatoion.Size = new System.Drawing.Size(166, 22);
+            this.textBoxElevatoion.TabIndex = 8;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(27, 122);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(65, 17);
+            this.label6.TabIndex = 10;
+            this.label6.Text = "timezone";
+            // 
+            // textBoxTimezone
+            // 
+            this.textBoxTimezone.Location = new System.Drawing.Point(101, 119);
+            this.textBoxTimezone.Name = "textBoxTimezone";
+            this.textBoxTimezone.Size = new System.Drawing.Size(166, 22);
+            this.textBoxTimezone.TabIndex = 11;
+            // 
+            // textBoxInstall
+            // 
+            this.textBoxInstall.Location = new System.Drawing.Point(101, 145);
+            this.textBoxInstall.Name = "textBoxInstall";
+            this.textBoxInstall.Size = new System.Drawing.Size(166, 22);
+            this.textBoxInstall.TabIndex = 13;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(27, 148);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(44, 17);
+            this.label7.TabIndex = 12;
+            this.label7.Text = "install";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dataGridView1.Location = new System.Drawing.Point(3, 84);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(660, 426);
+            this.dataGridView1.TabIndex = 4;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.textBoxdescription);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.textBoxSiteID);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(3, 3);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(660, 81);
+            this.panel1.TabIndex = 5;
+            // 
+            // AddSite
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(674, 542);
+            this.Controls.Add(this.tabControl1);
+            this.Name = "AddSite";
+            this.Text = "AddSite";
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage2.ResumeLayout(false);
+            this.tabPage2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBoxSiteID;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBoxdescription;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBoxlatitude;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBoxlongitude;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.TextBox textBoxInstall;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox textBoxTimezone;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBoxElevatoion;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Panel panel1;
+    }
+}
