a3f1b4ea263be834978a5c24ec4db523c12152d1
Karl Tarbet <ktarbet@users.noreply.github.com>
2015-06-01 08:41:25 -0600

    download NuGet.exe if missing

--- linux-build.sh
+++ linux-build.sh
@@ -1,5 +1,19 @@
 #!/bin/bash
-cd /data/apps/pisces/src
+. /opt/mono/env.sh
+cd /data/apps/pisces/src/Pisces
+
+if [ -f ./.nuget/NuGet.exe ]; then
+   echo "using existing NuGet.exe"
+else
+wget http://nuget.org/nuget.exe -O ./.nuget/NuGet.exe
+chmod a+x ./.nuget/NuGet.exe
+sudo mozroots --import --machine --sync
+sudo certmgr -ssl -m https://go.microsoft.com
+sudo certmgr -ssl -m https://nugetgallery.blob.core.windows.net
+sudo certmgr -ssl -m https://nuget.org   
+fi
+
+
 X=xbuild
 #echo "checking https://github.com/usbr/Pisces"
 #git pull
