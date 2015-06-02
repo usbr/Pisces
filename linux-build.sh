d6571ed3fa952b22734712f9fe68de45a2b2f649
Karl Tarbet <ktarbet@users.noreply.github.com>
2015-06-02 13:51:18 -0600

    Update linux-build.sh

--- linux-build.sh
+++ linux-build.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-. /opt/mono/env.sh
+export MONO1=/opt/mono/bin
+. $MONO1/env.sh
 cd /data/apps/pisces/src/Pisces
 
 if [ -f ./.nuget/NuGet.exe ]; then
@@ -7,13 +8,13 @@ if [ -f ./.nuget/NuGet.exe ]; then
 else
 wget http://nuget.org/nuget.exe -O ./.nuget/NuGet.exe
 chmod a+x ./.nuget/NuGet.exe
-sudo mozroots --import --machine --sync
-sudo certmgr -ssl -m https://go.microsoft.com
-sudo certmgr -ssl -m https://nugetgallery.blob.core.windows.net
-sudo certmgr -ssl -m https://nuget.org   
+sudo $MONO1/mozroots --import --machine --sync
+sudo $MONO1/certmgr -ssl -m https://go.microsoft.com
+sudo $MONO1/certmgr -ssl -m https://nugetgallery.blob.core.windows.net
+sudo $MONO1/certmgr -ssl -m https://nuget.org   
 fi
 
-
+export PATH=$PATH:$MONO1
 X=xbuild
 #echo "checking https://github.com/usbr/Pisces"
 #git pull
@@ -21,4 +22,4 @@ export EnableNuGetPackageRestore=true
 $X ./Core/Reclamation.Core.csproj
 $X ./TimeSeries/Reclamation.TimeSeries.csproj
 $X ./HydrometServer/HydrometServer.csproj
-$S ./PiscesWebServices/PiscesWebServices.csproj
+$X ./PiscesWebServices/PiscesWebServices.csproj
