27093c6e2790945a50780c7da6b8bb40ebe1aa06
Bob Lounsbury <blounsbury@usbr.gov>
2015-06-19 13:07:03 -0600

    Added 2D interpolation function FileLookupInterpolate2D (bilinear interpolation). Closes #29.

--- TimeSeries/Math.TableLookup.cs
+++ TimeSeries/Math.TableLookup.cs
@@ -1,17 +1,16 @@
-﻿using Reclamation.TimeSeries.Parser;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿using System;
+using System.Data;
+using System.IO;
+using Reclamation.Core;
+using Reclamation.TimeSeries.Parser;
 
 namespace Reclamation.TimeSeries
 {
     public partial class Math
     {
 
-
         [FunctionAttribute("Performs simple Rating table lookup from a file",
-      "FileRatingTable(series1,\"table.csv\")")]
+            "FileRatingTable(series1,\"table.csv\")")]
         public static Series FileRatingTable(Series s, string fileName)
         {
 
@@ -20,5 +19,240 @@ namespace Reclamation.TimeSeries
             return rval;
 
         }
+
+        [FunctionAttribute("Performs 2D lookup from a file with linear interpolation in both directions",
+            "FileLookupInterpolate2D(series1, series2, csvFile)")]
+        public static Series FileLookupInterpolate2D(Series s1, Series s2, string fileName)
+        {
+            var rval = new Series();
+
+            if (s1.IsEmpty || s2.IsEmpty)
+            {
+                Logger.WriteLine("FileLookupInterpolate2D - input series empty");
+                return rval;
+            }
+
+            if (!File.Exists(fileName))
+            {
+                Logger.WriteLine("FileLookupInterpolate2D - input fileName, file not found");
+                return rval;
+            }
+
+            CsvFile csv = new CsvFile(fileName, CsvFile.FieldTypes.AllText);
+            foreach (var pt in s1)
+            {
+                Point point = pt;
+
+                var s2_idx = s2.IndexOf(pt.DateTime);
+                if (s2_idx < 0)
+                {
+                    point.Value = Point.MissingValueFlag;
+                }
+                else
+                {
+                    point.Value = Interpoloate2D(csv, pt.Value, s2[s2_idx].Value);
+                }
+                rval.Add(point);
+            }
+
+            return rval;
+        }
+
+        public static double Interpoloate2D(DataTable tbl, double rowValue, double columnValue)
+        {
+            int row = RowIndexValue(tbl, rowValue);
+            int col = ColumnIndexValue(tbl, columnValue);
+
+            string errmsg = "Error: Interpolate2D() row = " + row + " col = " + col;
+            if (row < 0 || col < 1)
+            {
+                Logger.WriteLine(errmsg);
+                return Point.MissingValueFlag;
+            }
+
+            // if first row or first column do linear interpolation
+            if (row == 0)
+            {
+                return InterpolateRow(tbl, row, columnValue);
+            }
+
+            if (col == 1)
+            {
+                double val = InterpolateColumn(tbl, col, rowValue);
+                if (val < 0)
+                {
+                    Logger.WriteLine(errmsg);
+                    Logger.WriteLine("row value: " + rowValue + " outside of table rows");
+                    return Point.MissingValueFlag;
+                }
+                else
+                    return val;
+            }
+
+            /*
+             * Bilinear Interpolation Equation
+             *   where: rowValue = y
+             *          columnValue = x
+             *          row = y2
+             *          col = x2
+             * 
+             *        x1   |  x   |	 x2
+             * -------------------------
+               y1  |  Q11  |      |  Q21
+             * -------------------------
+               y   |         rval	
+             * -------------------------
+               y2  |  Q12  |      |  Q22
+             * -------------------------
+             * 
+             *            (x2 - x)(y2 - y)            (x - x1)(y2 - y)
+             *  rval ~=  ------------------ * Q11  +  ----------------- * Q21
+             *            (x2 - x1)(y2 - y1)          (x2 - x1)(y2 - y1)
+             * 
+             *            (x2 - x)(y - y1)            (x - x1)(y - y1)
+             *          + ----------------- * Q12  +  ----------------- * Q22
+             *            (x2 - x1)(y2 - y1)          (x2 - x1)(y2 - y1)
+             */
+
+            double y = rowValue;
+            double y1 = Convert.ToDouble(tbl.Rows[row - 1][0]);
+            double y2 = Convert.ToDouble(tbl.Rows[row][0]);
+            
+            // out of bounds, interpolation unknown
+            if (y > y2)
+            {
+                Logger.WriteLine(errmsg);
+                Logger.WriteLine("value outside of table rows");
+                return Point.MissingValueFlag;
+            }
+
+            double x = columnValue;
+            double x1 = Convert.ToDouble(tbl.Columns[col - 1].ColumnName);
+            double x2 = Convert.ToDouble(tbl.Columns[col].ColumnName);
+
+            // out of bounds, using last column of data for interpolation
+            if (x > x2)
+            {
+                Logger.WriteLine("Interpolate2D() using last column of data " +
+                                 "for interpolation, value provided greater" +
+                                 "than column max");
+                x = x2;
+            }
+
+            double Q11 = ValueAt(tbl, row - 1, col - 1);
+            double Q12 = ValueAt(tbl, row, col - 1);
+            double Q21 = ValueAt(tbl, row - 1, col);
+            double Q22 = ValueAt(tbl, row, col);
+
+            double denom = (x2 - x1) * (y2 - y1);
+
+            if (denom == 0)
+            {
+                Logger.WriteLine(errmsg);
+                Logger.WriteLine("error: division by zero");
+                return Point.MissingValueFlag;
+            }
+
+            double rval = ((x2 - x) * (y2 - y)) / denom * Q11;
+            rval += ((x - x1) * (y2 - y)) / denom * Q21;
+            rval += ((x2 - x) * (y - y1)) / denom * Q12;
+            rval += ((x - x1) * (y - y1)) / denom * Q22;
+
+            return rval;
+        }
+
+        private static double InterpolateRow(DataTable tbl, int row, double columnValue)
+        {
+            int col = ColumnIndexValue(tbl, columnValue);
+
+            // no interpolation needed, first value in table
+            if (row == 0 && col == 1)
+                return ValueAt(tbl, row, col);
+
+            double x = columnValue;
+            double x1 = Convert.ToDouble(tbl.Columns[col - 1].ColumnName);
+            double x2 = Convert.ToDouble(tbl.Columns[col].ColumnName);
+
+            // out of bounds, using last column of data for interpolation
+            if (x > x2)
+                x = x2;
+
+            double y1 = ValueAt(tbl, row, col - 1);
+            double y2 = ValueAt(tbl, row, col);
+
+            return Interpolate(x, x1, x2, y1, y2);
+        }
+
+        private static double InterpolateColumn(DataTable tbl, int col, double rowValue)
+        {
+            int row = RowIndexValue(tbl, rowValue);
+
+            // no interpolation needed, first value in table
+            if (row == 0 && col == 1)
+                return ValueAt(tbl, row, col);
+
+            double x = rowValue;
+            double x1 = ValueAt(tbl, row - 1, 0);
+            double x2 = ValueAt(tbl, row, 0);
+
+            // out of bounds, interpolation unknown
+            if (x > x2)
+                return -1;
+
+            double y1 = ValueAt(tbl, row - 1, col);
+            double y2 = ValueAt(tbl, row, col);
+
+            return Interpolate(x, x1, x2, y1, y2);
+        }
+
+        private static double Interpolate(double x, double x1, double x2, double y1, double y2)
+        {
+            if ((x2 - x1) == 0)
+            {
+                return (y1 + y2) / 2;
+            }
+            return y1 + (x - x1) * (y2 - y1) / (x2 - x1);
+        }
+
+        private static double ValueAt(DataTable tbl, int row, int col)
+        {
+            return Convert.ToDouble(tbl.Rows[row][col]);
+        }
+
+        private static int ColumnIndexValue(DataTable tbl, double columnValue)
+        {
+            for (int i = 1; i < tbl.Columns.Count; i++)
+            {
+                string cn = tbl.Columns[i].ColumnName;
+                double val = 0.0;
+                if (double.TryParse(cn, out val))
+                {
+                    if (val >= columnValue)
+                        return i;
+                }
+                else
+                {
+                    return -1;
+                }
+            }
+
+            return tbl.Columns.Count - 1;
+        }
+
+        private static int RowIndexValue(DataTable tbl, double rowValue)
+        {
+            for (int i = 0; i < tbl.Rows.Count; i++)
+            {
+                string cn = tbl.Rows[i][0].ToString();
+                double val = 0;
+                if (double.TryParse(cn, out val))
+                {
+                    if (val >= rowValue)
+                        return i;
+                }
+            }
+            return -1;
+        }
+
     }
 }
---
+++ TimeSeries/Tests/SeriesMath/TestInterpolate2D.cs
@@ -0,0 +1,108 @@
+﻿using System.IO;
+using NUnit.Framework;
+using Reclamation.Core;
+using Reclamation.TimeSeries;
+
+namespace Pisces.NunitTests.SeriesMath
+{
+    [TestFixture]
+    public class TestInterpolate2D
+    {
+        [Test]
+        public void Interpolate2D()
+        {
+            var tbl = TestData.RirieQ();
+
+            // sanity check, no iterpolation needed
+            double q1 = Reclamation.TimeSeries.Math.Interpoloate2D(tbl, 5023, 0.5);
+            Assert.AreEqual(55.0, System.Math.Round(q1, 1)); 
+
+            // random check
+            double q2 = Reclamation.TimeSeries.Math.Interpoloate2D(tbl, 5101.86, 0.01);
+            double q3 = Reclamation.TimeSeries.Math.Interpoloate2D(tbl, 5101.86, 0.26);
+            Assert.AreEqual(59.57, System.Math.Round(q2 + q3, 2));
+            double q4 = Reclamation.TimeSeries.Math.Interpoloate2D(tbl, 5101.89, 0.01);
+            double q5 = Reclamation.TimeSeries.Math.Interpoloate2D(tbl, 5101.89, 0.26);
+            Assert.AreEqual(59.58, System.Math.Round(q4 + q5, 2));
+
+            // bounds check, higher gate opening than in table, currently 
+            // allowed but maybe we don't want to allow this, gets max flow
+            // at elevation with max gate opening
+            double q6 = Reclamation.TimeSeries.Math.Interpoloate2D(tbl, 5023, 8);
+            Assert.AreEqual(924.0, System.Math.Round(q6, 1));
+
+            // bounds check, higher forebay than in table, invalid
+            double q7 = Reclamation.TimeSeries.Math.Interpoloate2D(tbl, 5119.1, 0.5);
+            Assert.AreEqual(Point.MissingValueFlag, System.Math.Round(q7, 1));
+
+            // max flow at max elevation and max opening
+            double q8 = Reclamation.TimeSeries.Math.Interpoloate2D(tbl, 5119, 7);
+            Assert.AreEqual(2125.0, System.Math.Round(q8, 1));
+        }
+
+
+        [Test]
+        public void Interpolate2DWithDatabase()
+        {
+            Logger.EnableLogger();
+            var fn = FileUtility.GetTempFileName(".pdb");
+            File.Delete(fn);
+
+            SQLiteServer svr = new SQLiteServer(fn);
+            var db = new TimeSeriesDatabase(svr );
+
+            var c = new CalculationSeries("rir_q");
+            var path = Path.Combine(Globals.TestDataPath, "rating_tables");
+            path = Path.Combine(path, "rir_q.txt");
+
+            c.Expression = "FileLookupInterpolate2D(rir_fb, rir_ra, \"" + path + "\")" +
+                " + FileLookupInterpolate2D(rir_fb, rir_rb, \"" + path + "\")";
+            c.TimeInterval = TimeInterval.Irregular;
+            db.AddSeries(c);
+
+            var fb = new Series("rir_fb");
+            fb.TimeInterval = TimeInterval.Irregular;
+            db.AddSeries(fb);
+            fb.Add("6-1-2011", 5110.99);
+            fb.Add("6-2-2011", 5111.31);
+            fb.Add("6-3-2011", 5111.71);
+            fb.Add("6-4-2011", 5112.09);
+
+            var ra = new Series("rir_ra");
+            ra.TimeInterval = TimeInterval.Irregular;
+            ra.Add("6-1-2011", 2.1);
+            ra.Add("6-2-2011", 1.29);
+            ra.Add("6-3-2011", 1.29);
+            ra.Add("6-4-2011", 1.29);
+            db.AddSeries(ra);
+
+            var rb = new Series("rir_rb");
+            rb.TimeInterval = TimeInterval.Irregular;
+            rb.Add("6-1-2011", 2.1);
+            rb.Add("6-2-2011", 1.28);
+            rb.Add("6-3-2011", 1.28);
+            rb.Add("6-4-2011", 1.28);
+            db.AddSeries(rb);
+
+            TimeSeriesImporter ti = new TimeSeriesImporter(db);
+            ti.Import(fb, computeDependencies: true);// this should force a calculation...
+
+            var q = db.GetSeriesFromTableName("rir_q");
+            Assert.NotNull(q, "Series not created");
+
+            q.Read();
+
+            /* 
+             * Flows from Hydromet
+             * 6-1-2011, 1009.87
+             * 6-2-2011, 602.24
+             * 6-3-2011, 603.32
+             * 6-4-2011, 604.34
+             */
+
+            Assert.AreEqual(4, q.Count);
+            Assert.AreEqual(1009.87, System.Math.Round(q[0].Value, 2));
+            Assert.AreEqual(603.32, System.Math.Round(q[2].Value, 2));
+        }
+    }
+}
