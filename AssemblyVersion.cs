94e2c9c182399ecfdacd13567dc42a5c6e567b7b
ktarbet <ktarbet@gmail.com>
2016-01-19 14:07:55 -0700

    2.0.11

--- AssemblyVersion.cs
+++ AssemblyVersion.cs
@@ -1,3 +1,3 @@
 using System.Reflection;
-[assembly: AssemblyVersion("2.0.10")]
-[assembly: AssemblyFileVersion("2.0.10")]
+[assembly: AssemblyVersion("2.0.11")]
+[assembly: AssemblyFileVersion("2.0.11")]
