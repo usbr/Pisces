77b75357a466e89daddf54651c87d91902cae38f
jhildreth1 <jhildreth@usbr.gov>
2016-02-16 10:09:49 -0700

    Fixed test to check for bug

--- TimeSeries/Tests/SeriesFeatures/TestShiftSeriesBug.cs
+++ TimeSeries/Tests/SeriesFeatures/TestShiftSeriesBug.cs
@@ -26,13 +26,37 @@ namespace Pisces.NunitTests.SeriesFeatures
                 t = t.AddDays(1);
             } while (t <= t2);
 
+            //check from non leap year
            var s2 = Reclamation.TimeSeries.Math.ShiftToYear(s, 2008);
-            var dec31 = s2[s2.Count -1];
 
-            Assert.AreEqual(2016, dec31.DateTime.Year);
-            s2.WriteToConsole();
+            s2 = Reclamation.TimeSeries.Math.ShiftToYear(s, 2016);
 
+            s2 = Reclamation.TimeSeries.Math.ShiftToYear(s, 2009);
+
+            s2 = Reclamation.TimeSeries.Math.ShiftToYear(s, 2017);
+
+            Series s3 = new Series();
+
+            t = new DateTime(2016, 2, 28);
+            t2 = new DateTime(2016, 12, 31);
+
+
+            do
+            {
+                s2.Add(t, t.Day);
+                t = t.AddDays(1);
+            } while (t <= t2);
+
+            //check from leap year
+            s2 = Reclamation.TimeSeries.Math.ShiftToYear(s, 2008);
+
+            s2 = Reclamation.TimeSeries.Math.ShiftToYear(s, 2020);
+
+            s2 = Reclamation.TimeSeries.Math.ShiftToYear(s, 2009);
+
+            s2 = Reclamation.TimeSeries.Math.ShiftToYear(s, 2017);
         }
+
     }
 }
 
