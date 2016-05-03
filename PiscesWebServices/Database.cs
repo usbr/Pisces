1c09b64e8619fdbe2f36e24efbcd968a47e432e3
ktarbet <ktarbet@usbr.gov>
2016-05-03 15:50:08 -0600

    Add initial scenario object model

---
+++ PiscesWebServices/Database.cs
@@ -0,0 +1,27 @@
+﻿using Reclamation.Core;
+using Reclamation.TimeSeries;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PiscesWebServices
+{
+    /// <summary>
+    /// Manage Connection to Pisces Database for webservices
+    /// </summary>
+    static class Database
+    {
+        private static TimeSeriesDatabase s_db;
+         public static void InitDB(string[] args)
+        {
+            s_db = TimeSeriesDatabase.InitDatabase(new Arguments(args));
+        }
+
+        public static TimeSeriesDatabase DB()
+        {
+            return s_db;
+        }
+
+    }
+}
