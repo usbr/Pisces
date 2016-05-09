a50928da84cacf373c2adf8ef4fff1f0491b4ff3
Jon Rocha <jrocha@usbr.gov>
2016-05-08 22:12:03 -0700

    working JS filter on the query list

--- PiscesWebServices/HomePage.cs
+++ PiscesWebServices/HomePage.cs
@@ -228,6 +228,41 @@ namespace PiscesWebServices
 @"});" +
 @"});" +
 @"</script>" +
+// Define list filter
+@"<script>" +
+@"    jQuery.fn.filterByText = function(textbox, selectSingleMatch) {" +
+@"        return this.each(function() {" +
+@"            var select = this;" +
+@"            var options = [];" +
+@"            $(select).find('option').each(function() {" +
+@"                options.push({value: $(this).val(), text: $(this).text()});" +
+@"            });" +
+@"            $(select).data('options', options);" +
+@"            $(textbox).bind('change keyup', function() {" +
+@"                var options = $(select).empty().data('options');" +
+@"                var search = $.trim($(this).val());" +
+@"                var regex = new RegExp(search,""gi"");" +
+@"              " +
+@"                $.each(options, function(i) {" +
+@"                    var option = options[i];" +
+@"                    if(option.text.match(regex) !== null) {" +
+@"                        $(select).append(" +
+@"                           $('<option>').text(option.text).val(option.value)" +
+@"                        );" +
+@"                    }" +
+@"                });" +
+@"                if (selectSingleMatch === true && $(select).children().length === 1) {" +
+@"                    $(select).children().get(0).selected = true;" +
+@"                }" +
+@"            });            " +
+@"        });" +
+@"    };" +
+@"" +
+@"    $(function() {" +
+@"        $('#tableName').filterByText($('#textbox'), true);" +
+@"    });  " +
+@"</script>" +
+
 
 @"<!-- Outlines -->" +
 @"<script src=""http://www.usbr.gov/js/components/outlines.js""></script>" +
@@ -264,8 +299,9 @@ namespace PiscesWebServices
 @"<strong>Select Pisces Data from the drop-down list: (sorted alphabetically by site name)</strong>" +
 @"" +
 @"<blockquote>" +
-//@"<input type=""text"" name=""tableName"" id=""tableName""><br>"+
-@"<select id=""tableName"" name=""tableName"" style=""width:300px"">" + 
+@"<input id=""textbox"" placeholder=""Type to filter"">"+
+@"<br><br>" +
+@"<select id=""tableName"" name=""tableName"" class=""List"" style=""width:500px"">" + 
 BuildQueryPageHtmlList() + 
 @"</select>" + 
 @"</blockquote>" +
@@ -309,8 +345,8 @@ BuildQueryPageHtmlList() +
             for (int i = 0; i < dTab.Rows.Count; i++)
             {
                 var dRow = dTab.Rows[i];
-                queryList += @"<option value=""" + dRow[0].ToString().Trim() + @"""" +
-                    @" label=""" + dRow[1] + " " + dRow[2] + @""">";
+                queryList += @"<option value=""" + dRow[0].ToString().Trim() + @""">" +
+                   dRow[1] + " " + dRow[2] + @"</option>";
             }
             return queryList;
         }
