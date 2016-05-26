a2942a2e562d63530a370b41f914bd99fadbec4d
robertherrera <rherrera@usbr.gov>
2016-05-26 09:45:34 -0600

    creates call groups

--- Asterisk/ExtensionBuilder/src/ExtensionsConfig.java
+++ Asterisk/ExtensionBuilder/src/ExtensionsConfig.java
@@ -4,81 +4,64 @@ public class ExtensionsConfig {
 
 	public static void main(String[] args) {
 
-		
-		CallOutLists c = new CallOutLists();
-		
-		System.out.println("there are "+c.getGroupNames().length+ " groups");
-		
-		String group1 = c.getGroupNames()[0];
+
+		CallOutLists list = new CallOutLists();
+
+		System.out.println("there are "+list.getGroupNames().length+ " groups");
+
+		String group1 = list.getGroupNames()[0];
 		System.out.println("numbers for group "+group1);
-		
-		
-		String[] numbers = c.GetPhoneNumbers(group1);
-			for (int i = 0; i < numbers.length; i++) {
-				System.out.println(numbers[i]);
-			}
-		
-		
+
+
+		String[] numbers = list.GetPhoneNumbers(group1);
+		for (int i = 0; i < numbers.length; i++) {
+			System.out.println(numbers[i]);
+		}
+
+
 		try{
 			PrintWriter writer = new PrintWriter("extensions.conf", "UTF-8");
 
 			//start of the extensions.conf
-			writer.println("[LocalSets]\n" + 
-					"exten => 200,1,Playback(tt-weasels)\n" +
-					"same  => n,Hangup()\n" +
-					"exten => 210,1,DIAL(SIP/210)\n" +
-					"same  => n,Hangup()\n" +
-					"exten => 211,1,DIAL(SIP/211)\n" +
-					"same  => n,Hangup()\n" +
-					"exten => 212,1,DIAL(SIP/212)\n" +
-					"same  => n,Hangup()\n" +
-					"exten => 213,1,DIAL(SIP/213)\n" +
-					"same  => n,Hangup()\n");
+			writer.println("[hydromet_groups]");
 
-			writer.println("[rob]\n" +
-					"exten => msg1,1,NoOp(originates a call to rob)\n" +
-					"same  => n,Originate(SIP/pn/6213,exten,almmsg,msg1,1,10)\n" +
-					"same  => n,GotoIf($[${ORIGINATE_STATUS} = SUCCESS]?end)\n" +
-					"same  => n(end),Hangup()\n");
+			for (int i = 0; i < list.getGroupNames().length; i++) {
+				writer.println("exten => " + list.getGroupNames()[i] + ",1,NoOp()");
+				if (i == 0) {
+					writer.println("same  => n,Set(${var}=wicews_gh_confirmed)\n" +
+							"same  => n,Set(DB(hydromet/${var})=0)");
+				}
+				for (int j = 0; j < list.GetPhoneNumbers(list.getGroupNames()[i]).length; j++) {
+					writer.println("same  => n,Originate(SIP/pn/" + list.GetPhoneNumbers(list.getGroupNames()[i])[j] +
+							",exten,hydromet_alarm,alarmmsg,1,10)\n" +
+							"same  => n,GotoIf($[${DB(hydromet/${var})} = 1]?end)\n"+
+							"same  => n(end),Hangup()");
+				}
+			}
+			writer.println();
+			writer.println("[hydromet_alarm]\n"+
+					"exten => alarmmsg,1,NoOp()\n"+
+					"same  => n,Wait(2)\n"+
+					"same  => n,Playback(hydromet/hydrometintro)\n"+
+					"same  => n,Playback(hydromet/wicews_gh)\n"+
+					"same  => n,Playback(hydromet/value)\n"+
+					"same  => n,SayAlpha(${DB(hydromet/wicews_gh)})\n"+
+					"same  => n,Read(ackDigit,hydromet/press1,1,,2,5)\n"+
+					"same  => n,Gotoif($[\"${ackDigit}\" = \"1\"]?ackalert)\n"+
+					"same  => n,Wait(5)\n"+
+					"same  => n(ackalert),NoOp()\n"+
+					"same  => n,Read(ackDigit,hydromet/acknowledging,1,,2,5)\n"+
+					"same  => n,Gotoif($[\"${ackDigit}\" = \"1\"]?confirmed)\n"+
+					"same  => n,Goto(unconfirmed)\n"+
+					"same  => n(confirmed),NoOp()\n"+
+					"same  => n,Playback(vm-goodbye)\n"+
+					"same  => n,Set(DB(hydromet/${var})=1)\n"+
+					"same  => n,Hangup()\n"+
+					"same  => n(unconfirmed),NoOp()\n"+
+					"same  => n,Playback(vm-goodbye)");
 
-			writer.println("[dectalk]\n" +
-					"exten => robalarm,1,NoOp(calls karl and rob)\n" +
-					"same  => n,Originate(SIP/pn/6213,exten,almmsg,msg1,1,10)\n" +
-					"same  => n,GotoIf($[${ORIGINATE_STATUS} = SUCCESS]?end)\n" +
-					"same  => n,Originate(SIP/pn/5272,exten,almmsg,msg1,1,10)\n" +
-					"same  => n,GotoIf($[${ORIGINATE_STATUS} = SUCCESS]?end)\n" +
-					"same  => n(end),Hangup()\n" +
-					"exten => billalarm,1,NoOp(calls bill)\n" +
-					"same  => n,Originate(SIP/pn/5278,exten,almmsg,msg1,1,10)\n" +
-					"same  => n,GotoIf($[${ORIGINATE_STATUS} = SUCCESS]?end)\n" +
-					"same  => n(end),Hangup()\n");
-
-			writer.println("[almmsg]\n" +
-					"exten => msg1,1,NoOp(first test alarm message)\n" +
-					"same  => n,Wait(2)\n" +
-					"same  => n,Playback(hydromet/hydrometintro)\n" +
-					"same  => n,Playback(hydromet/wicews_gh)\n" +
-					"same  => n,Playback(hydromet/value)\n" +
-					"same  => n,SayAlpha(${DB(app_user/rob_fb)})\n" +
-					"same  => n,Read(ackDigit,hydromet/press1,1,,2,5)\n" +
-					"same  => n,Gotoif($[\"${ackDigit}\" = \"1\"]?ackalert)\n" +
-					"same  => n,Wait(5)\n" +
-					"\n" +
-					"same  => n(ackalert),NoOp(Hydromet alert acknowledgement)\n" +
-					"same  => n,Read(ackDigit,hydromet/acknowledging,1,,2,5)\n" +
-					"same  => n,Gotoif($[\"${ackDigit}\" = \"1\"]?confirmed)\n" +
-					"same  => n,Goto(unconfirmed)\n" +
-					"\n" +
-					"same  => n(confirmed),NoOp(Hydromet alarm confirmed)\n" +
-					"same  => n,Playback(vm-goodbye)\n" +
-					"same  => n,Hangup()\n" +
-					"\n" +
-					"same  => n(unconfirmed),NoOp(Hydromet alarm not confirmed)\n" +
-					"same  => n,Playback(tt-weasels)\n" +
-					"same  => n,Hangup()\n");
-
-					writer.close();
-					System.out.println("updated extensions.conf");
+			writer.close();
+			System.out.println("updated extensions.conf");
 
 		}catch(Exception e){
 			System.out.println("An Error Occured creating the file: " + e);
