e982ea70f9d614d74ea85d2345632260e6f3988f
ktarbet <ktarbet@usbr.gov>
2016-05-28 22:14:57 -0600

    setup gridlines and sample data

--- TimeSeries.Forms/Graphing/RatingTableZedGraph.cs
+++ TimeSeries.Forms/Graphing/RatingTableZedGraph.cs
@@ -6,6 +6,7 @@ using System.Data;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
+using ZedGraph;
 
 namespace Reclamation.TimeSeries.Graphing
 {
@@ -14,12 +15,71 @@ namespace Reclamation.TimeSeries.Graphing
         string title = "";
         string subTitle = "";
 
+        GraphPane mypane;
         public RatingTableZedGraph()
         {
             InitializeComponent();
+            mypane = chart1.GraphPane;
+            mypane.XAxis.Type = AxisType.Log;
+            mypane.AxisChange();
+            mypane.XAxis.MajorGrid.IsVisible = true;
+
+            SetGridsON();
+
+            //mypane.XAxis.Scale.Min = 1;
+            //mypane.XAxis.Scale.Max = 10000;
+            mypane.XAxis.Title.Text = "Flow (cfs)";
+
+            mypane.YAxis.Type = AxisType.Log;
+            //mypane.YAxis.Scale.Min = 1;
+            //mypane.YAxis.Scale.Max = 10000;
+            mypane.YAxis.Title.Text = "Stage (feet)";
+            mypane.AxisChange();
+        }
+
+        private void SetGridsON( )
+        {
+            
+            mypane.XAxis.MinorGrid.IsVisible = true;
+            mypane.XAxis.MajorGrid.IsVisible = true;
+            mypane.XAxis.MajorGrid.Color = System.Drawing.Color.DarkGreen;
+            mypane.XAxis.MajorGrid.DashOff = 2.0f;
+            mypane.XAxis.MajorGrid.DashOn = 2.0f;
+            mypane.XAxis.MinorGrid.DashOn = 4;
+
+            mypane.YAxis.MinorGrid.IsVisible = true;
+            mypane.YAxis.MajorGrid.IsVisible = true;
+            mypane.YAxis.MajorGrid.Color = System.Drawing.Color.DarkGreen;
+            mypane.YAxis.MajorGrid.DashOff = 2.0f;
+            mypane.YAxis.MajorGrid.DashOn = 2.0f;
+            mypane.YAxis.MinorGrid.DashOn = 4;
         }
 
 
+        public void DrawDemo()
+        {
+            PointPairList points = new PointPairList();
+            points.Add(28.43, 856);
+	
+points.Add(28.16,719);
+points.Add(28.00,675);
+points.Add(28.31,820);
+points.Add(28.91,1160);
+points.Add(28.53,937);
+points.Add(29.23,1350);
+points.Add(30.05,1960);
+points.Add(34.45,8450);
+points.Add(31.57,3740);
+points.Add(31.03,3150);
+points.Add(31.40,3690);
+points.Add(31.74,4280);
+points.Add(28.68,1036);
+points.Add(28.20,767);
+
+chart1.GraphPane.AddCurve("YRWW", points, Color.Green);
+
+RefreshGraph();
+        }
 
         /// <summary>
         /// remove all series from the graph.
@@ -28,6 +88,7 @@ namespace Reclamation.TimeSeries.Graphing
         {
             title = "";
             subTitle = "";
+            
         }
 
 
