6112dfcb713260373df37ae49258ad03055a086b
ktarbet <ktarbet@usbr.gov>
2016-07-18 09:11:11 -0600

    TestDeleteDatabaseProperties demonstrates deleting a series where the properties are also deleted.   see issue #77

---
+++ TimeSeries/Tests/Database/TestDeleteDatabaseProperties.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using NUnit.Framework;
+using Reclamation.TimeSeries;
+using Reclamation.Core;
+using System.IO;
+using Math = Reclamation.TimeSeries.Math;
+using Reclamation.TimeSeries.Excel;
+using Reclamation.TimeSeries.Hydromet;
+
+namespace Pisces.NunitTests.Database
+{
+    [TestFixture]
+    public class TestDeleteDatabaseProperties
+    {
+
+
+        TimeSeriesDatabase db;
+
+        public TestDeleteDatabaseProperties()
+        {
+            string fn = Path.Combine(@"C:\temp", "factory.pdb");
+            FileUtility.GetTempFileNameInDirectory(@"C:\temp\", ".pdb");
+
+            SQLiteServer.CreateNewDatabase(fn);
+            SQLiteServer svr = new SQLiteServer(fn);
+            db = new TimeSeriesDatabase(svr, false);
+
+            string dataPath = TestData.DataPath;
+            Series s = new Series("Glomma River");
+            s.Properties.Set("elevation", " 690 m");
+
+            db.AddSeries(s);
+        }
+
+
+        [Test]
+        public void TestSeriesDeleteGlommaRiver()
+        {
+            var s = db.GetSeriesFromName("Glomma River");
+
+            db.Delete(s);
+
+            Assert.AreEqual(0, db.GetSeriesProperties().Count, "expect zero properties");
+
+        }
+
+    }
+}
