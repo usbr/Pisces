16db9efe9d374784b023ba70fcd915dba99ca871
tjrocha <jrocha@usbr.gov>
2016-08-03 10:44:51 -0700

    Added capability to generate log scale regression lines to rating table measurements

--- TimeSeries/Estimation/Regression.cs
+++ TimeSeries/Estimation/Regression.cs
@@ -8,8 +8,32 @@ namespace Reclamation.TimeSeries.Estimation
     public class Regression
     {
 
-        public static Tuple<double, double>[] SimpleLinearRegression(double[] xData, double[] yData)
+        public static Tuple<double, double>[] SimpleLinearRegression(double[] xDataIn, double[] yDataIn, 
+            bool logTransform = false, int logBase = 10)
         {
+            double[] xData, yData;
+            // Do a log transform on flow data
+            if (logTransform)
+            {
+                var xTemp = new List<double>();
+                var yTemp = new List<double>();
+                for (int i = 0; i < xDataIn.Count(); i++)
+                {
+                    if (yDataIn[i] > 0)
+                    {
+                        yTemp.Add(System.Math.Log(yDataIn[i], logBase));
+                        xTemp.Add(xDataIn[i]);
+                    }
+                }
+                xData = xTemp.ToArray();
+                yData = yTemp.ToArray();
+            }
+            else
+            {
+                xData = xDataIn;
+                yData = yDataIn;
+            }
+
             // produces an equation of the form Y = mX + b
             Tuple<double, double> coeffs = MathNet.Numerics.Fit.Line(xData, yData);
             double b = coeffs.Item1;
@@ -24,7 +48,15 @@ namespace Reclamation.TimeSeries.Estimation
             for (int i = 0; i < evalCount; i++)
             {
                 var xVal = xData.Min() + (xStep * i);
-                modelVals[i] = new Tuple<double, double>(xVal, MathNet.Numerics.Evaluate.Polynomial(xVal, new double[] {b, m}));
+                var yVal = MathNet.Numerics.Evaluate.Polynomial(xVal, new double[] { b, m });
+                if (logTransform)
+                {
+                    modelVals[i] = new Tuple<double, double>(xVal,System.Math.Pow(logBase,yVal));
+                }
+                else
+                {
+                    modelVals[i] = new Tuple<double, double>(xVal, yVal);
+                }
             }
 
             return modelVals;
