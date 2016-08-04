0fdda67cfae75ee4e9dc4a6d969d3975341cfa05
Karl Tarbet <ktarbet@users.noreply.github.com>
2016-08-04 14:18:23 -0600

    Update rwis_sync.sh

--- Rwis.Sync/rwis_sync.sh
+++ Rwis.Sync/rwis_sync.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 # update data from regional databases
 . /opt/mono/env.sh
+TMPDIR=/data/apps/Pisces/tmp
+export TMPDIR
+rm -fr /data/apps/Pisces/tmp
+mkdir -p /data/apps/Pisces/tmp
 cd /data/apps/Pisces
 mono /data/apps/Pisces/Rwis.Sync/bin/Debug/Rwis.Sync.exe --t1=lastyear > rwis_sync.log
 cp rwis_sync.log `date +%A`
