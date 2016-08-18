6259085f3cf91eecbb1c6b6212e73ea3b4b76b16
ktarbet <ktarbet@usbr.gov>
2016-08-18 16:43:14 -0600

    use %~dp0 (to get directory of batch file in windows)

--- security-scan.bat
+++ security-scan.bat
@@ -1,7 +1,8 @@
 path=%path%;\C:\Program Files\HP_Fortify\HP_Fortify_SCA_and_Apps_4.31\bin
 c:
-cd C:\Users\KTarbet\Documents\project\Pisces\Core\bin\Debug
+cd %~dp0
+cd Core\bin\Debug\
 sourceanalyzer -b cs-pisces -clean
-sourceanalyzer -b cs-pisces -vsversion 14.0 .
+sourceanalyzer -b cs-pisces -vsversion 12.0 -libdirs . Reclamation.Core.dll
 sourceanalyzer -b cs-pisces -scan -debug -logfile scanlog.txt -f results.fpr
 pause
