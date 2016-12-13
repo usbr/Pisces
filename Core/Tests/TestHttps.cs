04a3a5956f78ed32e7ccf99dbe02bbd64243b2da
karl tarbet <ktarbet@usbr.gov>
2016-12-13 11:34:06 -0700

    test case to demonstrate #103

---
+++ Core/Tests/TestHttps.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using NUnit.Framework;
+using System.IO;
+using System.Net;
+
+namespace Reclamation.Core.Tests
+{
+    [TestFixture]
+    public class TestHttps
+    {
+        [Test]
+        public void usbr_http()
+        {
+          var fn  = FileUtility.GetTempFileName(".html");
+           Web.GetFile("https://www.usbr.gov/",fn); // fails
+        }
+        [Test]
+        public void google_http()
+        {
+            var fn = FileUtility.GetTempFileName(".html");
+            Web.GetFile("https://www.google.com/", fn);  // Works
+        }
+
+    }
+}
