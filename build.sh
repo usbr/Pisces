e55807d613adf6c2a067c44bc0aaf37646ae0160
karl tarbet <ktarbet@usbr.gov>
2016-12-16 06:39:08 -0700

    update build.sh for centos 7

--- build.sh
+++ build.sh
@@ -8,19 +8,14 @@ else
    echo "creating bin directory"
    mkdir bin
 fi 
+cp /usr/lib/nuget/NuGet.exe ./.nuget/NuGet.exe
+cp /usr/lib/nuget/NuGet.Core.dll ./.nuget/
+export EnableNuGetPackageRestore=true
+#./.nuget/NuGet.exe install NUnit.Runners -Version 2.6.4 -OutputDirectory testrunner
+xbuild Pisces.sln /p:PostBuildEvent="" 
+
 echo "copying core output to bin" 
 cp $HYDROMET/Reclamation.TimeSeries.dll Reclamation.Core.dll ./bin
 cp $HYDROMET/HydrometServer.exe HydrometServer.exe.config ./bin
 cp $RWIS/Rwis.Sync.exe Rwis.Sync.exe.config ./bin
 echo "core output copied to bin"
-if [ -f ./.nuget/NuGet.exe ]; then
-   echo "using existing nuget.exe"
-else
-   echo "downloading nuget"
-wget http://nuget.org/nuget.exe
-cp nuget.exe ./.nuget/NuGet.exe
-chmod a+x ./.nuget/NuGet.exe
-fi 
-export EnableNuGetPackageRestore=true
-./.nuget/NuGet.exe install NUnit.Runners -Version 2.6.4 -OutputDirectory testrunner
-xbuild Pisces.sln /p:PostBuildEvent="" 
