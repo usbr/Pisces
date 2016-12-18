0f755ac9b636502e490a56f5a049c98e0e0c60fa
ktarbet <ktarbet@usbr.gov>
2016-12-17 18:39:43 -0700

    change hydromet to Hydromet (case sensitive Linux)

--- TimeSeries/AssemblyInfo.cs
+++ TimeSeries/AssemblyInfo.cs
@@ -3,12 +3,10 @@ using System.Runtime.CompilerServices;
 [assembly: AssemblyTitle("TimeSeries Library")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("ktarbet@pn.usbr.gov")]
+[assembly: AssemblyCompany("ktarbet@usbr.gov")]
 [assembly: AssemblyProduct("Reclamation.TimeSeries.dll")]
-[assembly: AssemblyCopyright("Copyright ©  2007")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]		
 
-[assembly: AssemblyVersion("1.0.6.0")]
 
 
