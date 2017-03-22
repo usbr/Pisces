52e6115354e43105e660d4a107afb9ec305527a1
hydromet <hydromet@usbr.gov>
2017-03-22 15:13:51 -0600

    fix ImapX reference path

--- build.sh
+++ build.sh
@@ -1,15 +1,23 @@
 #!/bin/bash
 # linux build script
-RWIS=./Rwis.Sync/bin/debug
-HYDROMET=./HydrometServer/bin/debug
+RWIS=./Applications/Rwis.Sync/bin/debug
+HYDROMET=./Applications/HydrometServer/bin/Debug
 if [ -e ./bin ]; then
    echo "using existing bin"
 else
    echo "creating bin directory"
    mkdir bin
 fi 
-cp /usr/lib/nuget/NuGet.exe ./.nuget/NuGet.exe
-cp /usr/lib/nuget/NuGet.Core.dll ./.nuget/
+
+if [ -f ./.nuget/nuget.exe ]; then
+   echo " nuget exists"
+else
+wget -O .nuget/nuget.exe https://dist.nuget.org/win-x86-commandline/latest/nuget.exe
+fi
+
+
+mono .nuget/nuget.exe restore Pisces.sln
+
 export EnableNuGetPackageRestore=true
 #./.nuget/NuGet.exe install NUnit.Runners -Version 2.6.4 -OutputDirectory testrunner
 xbuild Pisces.sln /p:PostBuildEvent="" 
