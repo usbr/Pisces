5eefb149f3199432d45449bf7d4777d8a3be09ac
karl tarbet <ktarbet@usbr.gov>
2017-05-09 11:36:08 -0600

    stubb for multi table saving

--- Core/SQLiteServer.cs
+++ Core/SQLiteServer.cs
@@ -405,6 +405,47 @@ namespace Reclamation.Core
             return recordCount;
         }
 
+        public int SaveTable(DataTable[] dataTable)
+        {
+            Performance perf = new Performance();
+            DataSet myDataSet = new DataSet();
+            
+            int recordCount = 0;
+
+            using (SQLiteConnection conn = new SQLiteConnection(ConnectionString))
+            using (SQLiteCommand myAccessCommand = new SQLiteCommand() )
+            using (SQLiteDataAdapter myDataAdapter = new SQLiteDataAdapter(myAccessCommand))
+            using (SQLiteCommandBuilder karlCB = new SQLiteCommandBuilder(myDataAdapter))
+            {
+
+                try
+                {
+                    conn.Open();
+                    
+                    var dbTrans = conn.BeginTransaction();
+
+                    for (int i = 0; i < dataTable.Length; i++)
+                    {
+                        var tn = dataTable[i].TableName;
+                        myDataSet.Tables.Add(tn);
+                        string sql = "select  * from " + PortableTableName(tn) + " where 2=1";
+                        myDataAdapter.SelectCommand = new SQLiteCommand(sql,conn);
+                        myDataAdapter.Fill(myDataSet, tn);
+                        recordCount = myDataAdapter.Update(dataTable[i]);
+                        myDataSet.Tables.Remove(tn);
+                    }
+                    dbTrans.Commit();
+                }
+                finally
+                {
+                    if (conn != null)
+                        conn.Close();
+                }
+            }
+
+            return recordCount;
+        }
+
 
         /// <summary>
         /// returns all rows from a table.
