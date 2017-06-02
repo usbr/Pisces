4eab6316b915d13151ec04a18898c31f7312360c
Karl Tarbet <ktarbet@gmail.com>
2017-06-02 14:32:13 -0600

    GetNiceNet downloads from http://www.wrcc.dri.edu/weather/

---
+++ Applications/GetNiceNet/Parameter.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GetNiceNet
+{
+    class Parameter
+    {
+        public double UpperLimitValue { get; set; }
+        public double LowerLimitValue { get; set; }
+        public string UpperLimitFlag { get; set; }
+        public string LowerLimitFlag { get; set; }
+        public string Units { get; set; }
+        public string Code { get; set; }
+        public string Name { get; set; }
+    }
+}
