6b024ab6a016118174996cd7f4a1b1559e111efe
tjrocha <jrocha@usbr.gov>
2017-06-03 08:54:08 -0700

    Enable built-in Nancy diagnostics @ http://hostname/_Nancy/

---
+++ Applications/PiscesWebServices/NancyBootstrapper.cs
@@ -0,0 +1,16 @@
+using Nancy;
+using Nancy.Diagnostics;
+
+namespace PiscesWebServices
+{
+    public class NancyBootstrapper : DefaultNancyBootstrapper
+    {
+        // The bootstrapper enables you to reconfigure the composition of the framework,
+        // by overriding the various methods and properties.
+        // For more information https://github.com/NancyFx/Nancy/wiki/Bootstrapper
+        protected override DiagnosticsConfiguration DiagnosticsConfiguration
+        {
+            get { return new DiagnosticsConfiguration { Password = @"secretpassword" }; }
+        }
+    }
+}
