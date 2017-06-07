09902014f10526ba325025fc8872bb4eea7d1a5b
tjrocha <jrocha@usbr.gov>
2017-06-07 09:32:48 -0700

    Add Response and Validation code files

---
+++ Applications/HdbAPI/Response.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Nancy;
+using Newtonsoft.Json;
+
+namespace HdbApi
+{
+    class ResponseHandler
+    {
+        public Response BuildErrorJson(string errorMessage, HttpStatusCode sCode)
+        {
+            var error = new ErrorReponse();
+            error.status = (int)sCode;
+            error.message = errorMessage;
+            error.requesttime = DateTime.Now;
+
+            var response = (Response)JsonConvert.SerializeObject(error);
+            response.StatusCode = sCode;
+            response.ContentType = "application/json";
+            return response;
+        }
+
+        public class ErrorReponse
+        {
+            public int status { get; set; }
+            public string message { get; set; }
+            public DateTime requesttime { get; set; }
+        }
+    }
+}
---
+++ Applications/HdbAPI/Validation.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.ComponentModel;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace HdbApi.Validation
+{
+    public class Validator
+    {
+        /// <summary>
+        /// Check API input by attempting to convert to a type
+        /// </summary>
+        /// <param name="inputName"></param>
+        /// <param name="input"></param>
+        /// <param name="targetType"></param>
+        /// <param name="result"></param>
+        /// <returns></returns>
+        public ValidationResult ValidateInput(string inputName, string input, Type targetType, ValidationResult result)
+        {
+            var tempValidation = new ValidationResult();
+            if (input == null)
+            {
+                tempValidation.IsValid = false;
+                tempValidation.ValidationMessage = inputName + " cannot be unassigned.";
+            }
+            else
+            {
+                try
+                {
+                    TypeDescriptor.GetConverter(targetType).ConvertFromString(input);
+                    tempValidation.IsValid = true;
+                }
+                catch
+                {
+                    tempValidation.IsValid = false;
+                    tempValidation.ValidationMessage = input + " is not a valid " + targetType.ToString() + ". ";
+                }
+            }
+
+            if (tempValidation.IsValid == false)
+            {
+                result.IsValid = false;
+                result.ValidationMessage += tempValidation.ValidationMessage;                
+                return result;
+            }
+            else
+            {
+                return result;
+            }
+        }
+
+        /// <summary>
+        /// Check API input using a predefined List
+        /// </summary>
+        /// <param name="inputName"></param>
+        /// <param name="input"></param>
+        /// <param name="targetList"></param>
+        /// <param name="result"></param>
+        /// <returns></returns>
+        public ValidationResult ValidateInput(string inputName, string input, List<string> targetList, ValidationResult result)
+        {
+            var tempValidation = new ValidationResult();
+            if (input == null)
+            {
+                tempValidation.IsValid = false;
+                tempValidation.ValidationMessage = inputName + " cannot be unassigned. ";
+            }
+            else
+            {
+                if (targetList.Contains(input))
+                {
+                    tempValidation.IsValid = true;
+                }
+                else
+                {
+                    tempValidation.IsValid = false;
+                    tempValidation.ValidationMessage = input + " is not a valid option in {" + string.Join(",", targetList) + "}. ";
+
+                }
+            }
+
+            if (tempValidation.IsValid == false)
+            {
+                result.IsValid = false;
+                result.ValidationMessage += tempValidation.ValidationMessage;                
+                return result;
+            }
+            else
+            {
+                return result;
+            }
+        }
+
+        /// <summary>
+        /// Check API input using Regular Expressions
+        /// </summary>
+        /// <param name="inputName"></param>
+        /// <param name="input"></param>
+        /// <param name="regexCheck"></param>
+        /// <param name="result"></param>
+        /// <returns></returns>
+        public ValidationResult ValidateInput(string inputName, string input, string regexCheck, ValidationResult result)
+        {
+            var tempValidation = new ValidationResult();
+            if (input == null)
+            {
+                tempValidation.IsValid = false;
+                tempValidation.ValidationMessage = inputName + " cannot be unassigned. ";
+            }
+            else
+            {
+                Regex regex = new Regex(@"^.*$");
+                if (regexCheck == "allnumbers")
+                {
+                    regex = new Regex(@"^-?[\d]?[,\d,]*[\d]$");
+                }
+                Match match = regex.Match(input);
+                if (match.Success)
+                {
+                    tempValidation.IsValid = true;
+                }
+                else
+                {
+                    tempValidation.IsValid = false;
+                    tempValidation.ValidationMessage = input + " failed the " + regexCheck + " check. ";
+
+                }
+            }
+
+            if (tempValidation.IsValid == false)
+            {
+                result.IsValid = false;
+                result.ValidationMessage += tempValidation.ValidationMessage;
+                return result;
+            }
+            else
+            {
+                return result;
+            }
+        }
+
+    }
+
+    /// <summary>
+    /// Class for validation results
+    /// </summary>
+    public class ValidationResult
+    {
+        public bool IsValid { get; set; } = true;
+        public string ValidationMessage { get; set; } = null;
+    }
+}
