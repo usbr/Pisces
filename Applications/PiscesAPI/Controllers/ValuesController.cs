a6dc306ffbc3e7cb1688b61e2a3431de68f923f5
tjrocha <jrocha@usbr.gov>
2017-10-30 20:43:45 -0700

    fully fleshed out sample end points; added swagger documentation and ui annotation.

--- Applications/PiscesAPI/Controllers/ValuesController.cs
+++ Applications/PiscesAPI/Controllers/ValuesController.cs
@@ -7,36 +7,66 @@ using Microsoft.AspNetCore.Mvc;
 namespace PiscesAPI.Controllers
 {
     [Route("api/[controller]")]
-    public class ValuesController : Controller
+    public class SampleEndPointsController : Controller
     {
-        // GET api/values
+        /// <summary>
+        /// Retrieves all products
+        /// </summary>
+        /// <remarks>Long description for this API endpoint goes here...</remarks>
+        /// <response code="200">Products fetched</response>
+        /// <response code="400">Products have missing/invalid values</response>
+        /// <response code="500">Oops! Can't fetch products right now</response>
         [HttpGet]
+        [ProducesResponseType(typeof(string), 200)]
+        [ProducesResponseType(typeof(string), 400)]
+        [ProducesResponseType(typeof(void), 500)]
         public IEnumerable<string> Get()
         {
             return new string[] { "value1", "value2" };
         }
 
-        // GET api/values/5
+        /// <summary>
+        /// Retrieves a specific product by unique id
+        /// </summary>
+        /// <remarks>Long description for this API endpoint goes here...</remarks>
+        /// <response code="200">Product fetched</response>
+        /// <response code="400">Product has missing/invalid values</response>
+        /// <response code="500">Oops! Can't fetch your product right now</response>
         [HttpGet("{id}")]
+        [ProducesResponseType(typeof(string), 200)]
+        [ProducesResponseType(typeof(string), 400)]
+        [ProducesResponseType(typeof(void), 500)]
         public string Get(int id)
         {
             return "value";
         }
 
-        // POST api/values
+        /// <summary>
+        /// Adds or modifies a specific product by unique id
+        /// </summary>
+        /// <remarks>Long description for this API endpoint goes here...</remarks>
+        /// <response code="200">Product(s) created</response>
+        /// <response code="400">Product(s) has missing/invalid values</response>
+        /// <response code="500">Oops! Can't create your product(s) right now</response>
         [HttpPost]
+        [ProducesResponseType(typeof(string), 200)]
+        [ProducesResponseType(typeof(string), 400)]
+        [ProducesResponseType(typeof(void), 500)]
         public void Post([FromBody]string value)
         {
         }
 
-        // PUT api/values/5
-        [HttpPut("{id}")]
-        public void Put(int id, [FromBody]string value)
-        {
-        }
-
-        // DELETE api/values/5
+        /// <summary>
+        /// Deletes a specific product by unique id
+        /// </summary>
+        /// <remarks>Long description for this API endpoint goes here...</remarks>
+        /// <response code="200">Product(s) deleted</response>
+        /// <response code="400">Product(s) has missing/invalid values</response>
+        /// <response code="500">Oops! Can't delete your product(s) right now</response>
         [HttpDelete("{id}")]
+        [ProducesResponseType(typeof(string), 200)]
+        [ProducesResponseType(typeof(string), 400)]
+        [ProducesResponseType(typeof(void), 500)]
         public void Delete(int id)
         {
         }
--- Applications/PiscesAPI/Startup.cs
+++ Applications/PiscesAPI/Startup.cs
@@ -8,6 +8,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using Swashbuckle.AspNetCore.Swagger;
 
 namespace PiscesAPI
 {
@@ -26,7 +27,38 @@ namespace PiscesAPI
             services.AddMvc();
             services.AddSwaggerGen(c =>
             {
-                c.SwaggerDoc("v1", new Swashbuckle.AspNetCore.Swagger.Info { Title = "Pisces API", Version = "v1" });
+                //c.SwaggerDoc("v0", new Swashbuckle.AspNetCore.Swagger.Info { Title = "Pisces API", Version = "v0" });
+                var filePath = System.IO.Path.Combine(Microsoft.Extensions.PlatformAbstractions.PlatformServices.Default.Application.ApplicationBasePath, "PiscesAPI.xml");
+                c.IncludeXmlComments(filePath);
+                c.SwaggerDoc("v0", new Info
+                {
+                    Title = "Pisces API Data Service",
+                    Version = "v0",
+                    Description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore " +
+                        "et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex " +
+                        "ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat " + 
+                        "nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim "+
+                        "id est laborum." +
+                        "\n\r" + 
+                        "This Application Programming Interface (API) is preliminary or provisional and is subject to revision. " +
+                        "It is currently in development and as such, frequent updates, downtimes, and loss of functionality are to " +
+                        "be expected. The API has not received final approval by Reclamation. No warranty, expressed or implied, is " +
+                        "made as to the functionality of the API nor shall the fact of release constitute any such warranty. " +
+                        "The API is provided on the condition that neither Reclamation nor the U.S. Government shall be held liable " +
+                        "for any damages resulting from the authorized or unauthorized use of the API.",
+                    TermsOfService = "TOS Placeholder",
+                    Contact = new Contact
+                    {
+                        //Name = "Jon Rocha",
+                        Email = "jrocha@usbr.gov"
+                    },
+                    License = new License
+                    {
+                        Name = "MIT License",
+                        Url = "https://opensource.org/licenses/MIT"
+                    }
+                });
+
             });
         }
 
@@ -42,7 +74,7 @@ namespace PiscesAPI
             app.UseSwagger();
             app.UseSwaggerUI(c =>
             {
-                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Pisces API V1");
+                c.SwaggerEndpoint("/swagger/v0/swagger.json", "Pisces API V0");
             });
         }
     }
