d622fed75ee9f44d9d95bdde3f0880a69ecd9874
Karl Tarbet <ktarbet@gmail.com>
2017-12-01 11:11:59 -0700

    command line forecast term feature

--- Applications/HydrometForecast/CommandLine.cs
+++ Applications/HydrometForecast/CommandLine.cs
@@ -1,4 +1,5 @@
 ﻿using Reclamation.Core;
+using Reclamation.TimeSeries.Hydromet;
 using System;
 using System.Collections.Generic;
 using System.IO;
@@ -15,7 +16,7 @@ namespace HydrometForecast
         static void Main(string[] args)
         {
 
-            if( args.Length != 5)
+            if (args.Length != 5 && args.Length != 2)
             {
                 Console.WriteLine("HydrometForecast.exe basin.csv date level look-ahead output");
                 Console.WriteLine("Where: ");
@@ -24,23 +25,41 @@ namespace HydrometForecast
                 Console.WriteLine("        level          -- subsequent conditions  1.0 normal  0.8 for 80%, etc... ");
                 Console.WriteLine("        look-ahead     -- perfect forecast 0 or 1");
                 Console.WriteLine("        output         -- filename for output");
-
                 Console.WriteLine("Example:  HydrometForecast  heise.csv 2016-1-1 1.0 0 output.txt");
+
+                Console.WriteLine("HydrometForecast.exe basin.csv output.csv");
+
                 return;
             }
 
             var filename = args[0];
-            DateTime t = DateTime.Parse(args[1]);
-            double level = double.Parse(args[2]);
-            int lookAhead = int.Parse(args[3]);
-            string outputFileName = args[4];
-
+            
 
             ForecastEquation eq = new ForecastEquation(filename);
-            ForecastResult result = eq.Evaluate(t,lookAhead == 1 ,level);
 
-            File.WriteAllLines(outputFileName, result.Details.ToArray());
+            if (args.Length == 2)
+            {
+                var cache = new HydrometDataCache();
 
+                cache.Add(eq.GetCbttPcodeList().ToArray(),
+                                      new DateTime(eq.StartYear - 1, 10, 1),
+                                      new DateTime(eq.EndYear, 9, 30));
+
+                HydrometMonthlySeries.Cache = cache;
+
+                var tbl = eq.ComputeHistoricalCoefficients(eq.StartYear, eq.EndYear);
+                CsvFile.WriteToCSV(tbl, args[1], false);
+            }
+            else
+            {
+                DateTime t = DateTime.Parse(args[1]);
+                double level = double.Parse(args[2]);
+                int lookAhead = int.Parse(args[3]);
+                string outputFileName = args[4];
+
+                ForecastResult result = eq.Evaluate(t, lookAhead == 1, level);
+                File.WriteAllLines(outputFileName, result.Details.ToArray());
+            }
         }
 
     }
