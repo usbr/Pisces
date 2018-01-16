usbr/Pisces

commit 72acc4cfd0ec4277a36bc68cd80d884b1776f9d2

fix SQL where clause

Core/CsvFile.cs
@@ -524,15 +524,37 @@ namespace Reclamation.Core
             Console.WriteLine(" done.");
         }
 
+
+        static StreamWriter s_sw = null;
+
+        private static void Write(string s)
+        {
+            if (s_sw != null)
+                s_sw.Write(s);
+            else
+                Console.Write(s);
+        }
+        private static void WriteLine(string s)
+        {
+            if (s_sw != null)
+                s_sw.Write(s);
+            else
+                Console.Write(s);
+        }
         /// <summary>
-        /// Saves contents of TextDB to comma seperated file.
+        /// Saves contents of DataTable to comma seperated file.
+        /// if filename is empty print to console instead of a file
         /// </summary>
         /// <param name="filename"></param>
         public static void WriteToCSV(DataTable table, string filename, bool WriteSchema=true, bool printHeader=true)
         {
             if (debugOutput)
                 Console.Write("Writing to " + filename);
-            StreamWriter sr = new StreamWriter(filename, false);
+            if (filename != "")
+                s_sw = new StreamWriter(filename, false);
+            else
+                s_sw = null;
+
             int sz = table.Rows.Count;
             int cols = table.Columns.Count;
             bool[] IsStringColumn = new Boolean[cols];
@@ -543,9 +565,9 @@ namespace Reclamation.Core
                 for (c = 0; c < cols; c++)
                 {
                     if (c < cols - 1)
-                        sr.Write(EncodeCSVCell(table.Columns[c].ColumnName.Trim()) + ",");
+                        Write(EncodeCSVCell(table.Columns[c].ColumnName.Trim()) + ",");
                     else
-                        sr.WriteLine(EncodeCSVCell(table.Columns[c].ColumnName.Trim())); // no comma on last
+                        WriteLine(EncodeCSVCell(table.Columns[c].ColumnName.Trim())); // no comma on last
                     if (table.Columns[c].DataType.ToString() == "System.String")
                         IsStringColumn[c] = true;
                 }
@@ -555,12 +577,12 @@ namespace Reclamation.Core
             {
                 for (c = 0; c < cols - 1; c++)
                 {
-                    sr.Write(table.Columns[c].DataType);
-                    sr.Write(",");
+                    Write(table.Columns[c].DataType.ToString());
+                    Write(",");
                 }
 
-                sr.Write(table.Columns[c].DataType); // no comma on last
-                sr.WriteLine();
+                Write(table.Columns[c].DataType.ToString()); // no comma on last
+                WriteLine("");
             }
 
             for (int r = 0; r < sz; r++)
@@ -572,19 +594,20 @@ namespace Reclamation.Core
                         string s = table.Rows[r][c].ToString();
                         // s = s.Replace("\"", "\"\"");
                         //sr.Write("\""+s+"\"");
-                        sr.Write(EncodeCSVCell(s));
+                        Write(EncodeCSVCell(s));
                     }
                     else
                     {
-                        sr.Write(table.Rows[r][c]);
+                        Write(table.Rows[r][c].ToString());
                     }
                     if (c < cols - 1)
-                        sr.Write(",");
+                        Write(",");
                 }
-                sr.WriteLine();
+                WriteLine("");
 
             }
-            sr.Close();
+            if( s_sw != null)
+              s_sw.Close();
             if (debugOutput)
                 Console.WriteLine(" done.");
         }

Applications/PiscesWebServices/Program.cs
@@ -5,6 +5,7 @@ using Reclamation.Core;
 using Reclamation.TimeSeries;
 using System;
 using System.Collections;
+using System.Data;
 
 namespace PiscesWebServices
 {
@@ -103,6 +104,11 @@ namespace PiscesWebServices
                     SiteCsvTable c = new SiteCsvTable(db);
                     c.Execute(siteType);
                 }
+                else if( format == "test")
+                {
+                    SiteCsvTable c = new SiteCsvTable(db);
+                    c.Execute(siteType,"jck");
+                }
             }
             else if (cgi == "instant" || cgi == "daily" || cgi == "monthly")
             {
@@ -175,6 +181,8 @@ namespace PiscesWebServices
             Console.WriteLine("Options:");
             p.WriteOptionDescriptions(Console.Out);
         }
+
+
 
     }
 }

Applications/PiscesWebServices/SiteCsvTable.cs
@@ -24,7 +24,7 @@ namespace PiscesWebServices
         /// <summary>
         /// Prints csv dump of Sites
         /// </summary>
-        public void Execute( string siteType="")
+        public void Execute( string siteType="", string siteid="")
         {
             Console.Write("Content-Type:  text/csv\n\n");
             Console.WriteLine("Content-disposition: attachment;filename=location.csv");
@@ -32,15 +32,23 @@ namespace PiscesWebServices
             var filter = "";
             if (siteType != "")
                 filter = "type = '" + siteType + "'";
+            if( siteid != "")
+            {
+                if (filter != "")
+                    filter += " and ";
+                filter += " siteid = '" + siteid + "'";
+
+            }
+
             var sites = db.GetSiteCatalog(filter:filter);
 
-            var fn = FileUtility.GetTempFileName(".csv");
-            CsvFile.WriteToCSV(sites, fn, false, true);
-            var lines = File.ReadAllLines(fn);
-            foreach (var item in lines)
-            {
-                Console.WriteLine(item);    
-            }
+            //var fn = FileUtility.GetTempFileName(".csv");
+            CsvFile.WriteToCSV(sites, "", false, true);
+            //var lines = File.ReadAllLines(fn);
+            //foreach (var item in lines)
+            //{
+            //    Console.WriteLine(item);    
+            //}
 
         }
     }

TimeSeries/TimeSeriesDatabase.cs
@@ -280,7 +280,9 @@ namespace Reclamation.TimeSeries
         public TimeSeriesDatabaseDataSet.sitecatalogDataTable GetSiteCatalog(string filter="1=1", string propertyFilter="")
         {
             var tbl = new TimeSeriesDatabaseDataSet.sitecatalogDataTable();
-            string sql = "select * from sitecatalog where " + filter;
+            string sql = "select * from sitecatalog ";
+            if( filter != "")
+                sql += " where " + filter;
             sql += GetSitePropertySQL(propertyFilter);
             sql += "  order by  siteid";
             m_server.FillTable(tbl, sql);

commit 0f492f550a68c3d766c2e647bb2919357978246c

fix csv file bug introduced in 72acc4cf

Core/CsvFile.cs
@@ -537,9 +537,9 @@ namespace Reclamation.Core
         private static void WriteLine(string s)
         {
             if (s_sw != null)
-                s_sw.Write(s);
+                s_sw.WriteLine(s);
             else
-                Console.Write(s);
+                Console.WriteLine(s);
         }
         /// <summary>
         /// Saves contents of DataTable to comma seperated file.