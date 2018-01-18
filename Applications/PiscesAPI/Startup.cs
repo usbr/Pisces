2f9efed98a9fbb6148a2c66e41b018f9cd7ccfa0
Karl Tarbet <ktarbet@gmail.com>
2018-01-18 08:44:43 -0700

    provide link to disclamer

--- Applications/PiscesAPI/Startup.cs
+++ Applications/PiscesAPI/Startup.cs
@@ -66,18 +66,7 @@ namespace PiscesAPI
                 {
                     Title = "Pisces API Data Service",
                     Version = versionName,
-                    Description = "Although the US Bureau of Reclamation makes efforts to maintain the accuracy of data found in the Hydromet system databases, " +
-                        "the data is largely unverified and should be considered preliminary and subject to change. Data and services are provided with the express " +
-                        "understanding that the United States Government makes no warranties, expressed or implied, concerning the accuracy, completeness, " +
-                        "usability or suitability for any particular purpose of the information or data obtained by access to this computer system, and the " +
-                        "United States shall be under no liability whatsoever to any individual or group entity by reason of any use made thereof." +
-                        "\n\r" +
-                        "This Application Programming Interface (API) is preliminary or provisional and is subject to revision. " +
-                        "It is currently in development and as such, frequent updates, downtimes, and loss of functionality are to " +
-                        "be expected. The API has not received final approval by Reclamation. No warranty, expressed or implied, is " +
-                        "made as to the functionality of the API nor shall the fact of release constitute any such warranty. " +
-                        "The API is provided on the condition that neither Reclamation nor the U.S. Government shall be held liable " +
-                        "for any damages resulting from the authorized or unauthorized use of the API.",
+                    Description = "https://www.usbr.gov/pn/hydromet/disclaimer.html",
                     TermsOfService = "TOS Placeholder",
                     Contact = new Contact
                     {
