8a2762c7f724edffb0581797eb8e97063bdb518e
Karl Tarbet <ktarbet@gmail.com>
2018-02-08 10:33:53 -0700

    Instant and Monthly API legacy support

--- Applications/PiscesAPI/Controllers/MonthlyController.cs
+++ Applications/PiscesAPI/Controllers/MonthlyController.cs
@@ -9,11 +9,11 @@ using Reclamation.Core;
 
 namespace PiscesAPI.Controllers
 {
-    [Route("instant/")]
-    public class InstantController : Controller
+    [Route("monthly/")]
+    public class MonthlyController : Controller
     {
         /// <summary>
-        /// Retrieve instant time (typically 15 minute) series data
+        /// Retrieve monthly time series data
         /// </summary>
         [HttpGet()]
         [ProducesResponseType(typeof(string), 200)]
@@ -25,8 +25,9 @@ namespace PiscesAPI.Controllers
             if (query.IndexOf("?") == 0)
                 query = query.Substring(1);
 
+
             var db = Database.GetTimeSeriesDatabase();
-            var w = new WebTimeSeriesWriter(db, Reclamation.TimeSeries.TimeInterval.Irregular, query);
+            var w = new WebTimeSeriesWriter(db, Reclamation.TimeSeries.TimeInterval.Monthly,query);
             var x = w.Run(Response);
             return Content(x, Response.ContentType);
         }
@@ -40,7 +41,7 @@ namespace PiscesAPI.Controllers
         [ProducesResponseType(typeof(string), 200)]
         [ProducesResponseType(typeof(string), 400)]
         [ProducesResponseType(typeof(void), 500)]
-        public string Post()
+        public ContentResult Post()
         {
             var sr = new System.IO.StreamReader(Request.Body);
             var body = sr.ReadToEnd();
@@ -49,10 +50,9 @@ namespace PiscesAPI.Controllers
             if (body == "")
                 throw new Exception("no data posted");
             var db = Database.GetTimeSeriesDatabase();
-            var w = new WebTimeSeriesWriter(db, Reclamation.TimeSeries.TimeInterval.Irregular, body);
-            var x = w.Run(Response);
-            return x;
-
+            var w = new WebTimeSeriesWriter(db, Reclamation.TimeSeries.TimeInterval.Monthly, body);
+            var x =w.Run(Response);
+            return Content(x, Response.ContentType);
         }
 
     }
