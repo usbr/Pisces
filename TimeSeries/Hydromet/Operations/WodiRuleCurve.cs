f7562ab295de1133f122346bbfe4ed4d23248e87
Karl Tarbet <ktarbet@gmail.com>
2018-03-02 10:50:32 -0700

    use daily data for little wood river inflow average

--- TimeSeries/Hydromet/Operations/WodiRuleCurve.cs
+++ TimeSeries/Hydromet/Operations/WodiRuleCurve.cs
@@ -1,9 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using Reclamation.TimeSeries;
-using Reclamation.TimeSeries.Hydromet;
+﻿using Reclamation.Core;
+using Reclamation.TimeSeries.Analysis;
+using System;
 using System.Data;
 
 namespace Reclamation.TimeSeries.Hydromet.Operations
@@ -46,10 +43,10 @@ namespace Reclamation.TimeSeries.Hydromet.Operations
 
         private void ReadQUAverage()
         {
-            var t1 = new DateTime(9999, 1, 1);
-            var t2 = new DateTime(9999, 9, 30);
-            quAverage = new HydrometMonthlySeries("wodi", "qua");
-            quAverage.Read(t1, t2);
+            var qua = AverageSeries30Year();
+            // we want january --> september (remove other months)..           
+            MonthDayRange r = new MonthDayRange(1, 1, 9, 30);
+            quAverage = Math.Subset(qua, r);
             quSumJuly = quAverage.Clone();
             
             for (int i = 0; i < 7; i++)
@@ -65,6 +62,18 @@ namespace Reclamation.TimeSeries.Hydromet.Operations
            quAverage = Reclamation.TimeSeries.Math.ShiftToYear(quAverage, waterYear);
         }
 
+        static Series AverageSeries30Year()
+        {
+            var t1 = HydrometDataUtility.T1Thirty;
+            var t2 = HydrometDataUtility.T2Thirty;
+            Series qu = new HydrometDailySeries("wodi", "qu");
+            qu.Read(t1, t2);
+            qu = Math.MonthlySum(qu) * 0.00198347;
+
+            var qua = MonthlySummaryAnalysis.MonthlySeries(qu, StatisticalMethods.Mean, false);
+            return qua;
+
+        }
 
         public override double LookupRequiredSpace(DateTime t, double resid, out string flag)
         {
