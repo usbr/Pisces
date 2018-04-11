cdcf4c4ac641493a0c12f83ec9d3b3fe32670772
Karl Tarbet <ktarbet@gmail.com>
2018-04-11 13:55:11 -0600

    estimated data is not considerd 'missing'

--- TimeSeries/Hydromet/HydrometInstantSeries.cs
+++ TimeSeries/Hydromet/HydrometInstantSeries.cs
@@ -12,7 +12,7 @@ namespace Reclamation.TimeSeries.Hydromet
     /// </summary>
     public class HydrometInstantSeries : Reclamation.TimeSeries.Series
     {
-        static string[] GoodDataFlags = new string[] { "", " ", PointFlag.Edited,PointFlag.Computed };
+        static string[] GoodDataFlags = new string[] { "", " ", PointFlag.Edited,PointFlag.Computed, PointFlag.Estimated };
         private string pcode;
         private string cbtt;
         private static bool s_keepflaggedData = false;// flagged data
