6d5ad23d91f2425e607744df61d6d1ed3fcb0a7a
Karl Tarbet <ktarbet@gmail.com>
2018-06-25 11:26:18 -0600

    remove color 'Beige'

--- TimeSeries.Forms/Properties/Settings.Designer.cs
+++ TimeSeries.Forms/Properties/Settings.Designer.cs
@@ -12,7 +12,7 @@ namespace Reclamation.TimeSeries.Forms.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "12.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "15.6.0.0")]
     internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
         
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
@@ -41,56 +41,56 @@ namespace Reclamation.TimeSeries.Forms.Properties {
             "org/2001/XMLSchema-instance\" xmlns:xsd=\"http://www.w3.org/2001/XMLSchema\">\r\n  <s" +
             "tring>Red</string>\r\n  <string>Green</string>\r\n  <string>Blue</string>\r\n  <string" +
             ">Black</string>\r\n  <string>Orange</string>\r\n  <string>Aquamarine</string>\r\n  <st" +
-            "ring>Beige</string>\r\n  <string>DarkGreen</string>\r\n  <string>Purple</string>\r\n  " +
-            "<string>Aqua</string>\r\n  <string>Bisque</string>\r\n  <string>BlanchedAlmond</stri" +
-            "ng>\r\n  <string>BlueViolet</string>\r\n  <string>Brown</string>\r\n  <string>BurlyWoo" +
-            "d</string>\r\n  <string>CadetBlue</string>\r\n  <string>Chartreuse</string>\r\n  <stri" +
-            "ng>Chocolate</string>\r\n  <string>Coral</string>\r\n  <string>CornflowerBlue</strin" +
-            "g>\r\n  <string>Cornsilk</string>\r\n  <string>Crimson</string>\r\n  <string>Cyan</str" +
-            "ing>\r\n  <string>DarkBlue</string>\r\n  <string>DarkCyan</string>\r\n  <string>DarkGo" +
-            "ldenrod</string>\r\n  <string>DarkGray</string>\r\n  <string>DarkKhaki</string>\r\n  <" +
-            "string>DarkMagenta</string>\r\n  <string>AntiqueWhite</string>\r\n  <string>DarkOliv" +
-            "eGreen</string>\r\n  <string>DarkOrange</string>\r\n  <string>DarkOrchid</string>\r\n " +
-            " <string>DarkRed</string>\r\n  <string>DarkSalmon</string>\r\n  <string>DarkSeaGreen" +
-            "</string>\r\n  <string>DarkSlateBlue</string>\r\n  <string>DarkSlateGray</string>\r\n " +
-            " <string>DarkTurquoise</string>\r\n  <string>DarkViolet</string>\r\n  <string>DeepPi" +
-            "nk</string>\r\n  <string>DeepSkyBlue</string>\r\n  <string>DimGray</string>\r\n  <stri" +
-            "ng>DodgerBlue</string>\r\n  <string>Firebrick</string>\r\n  <string>FloralWhite</str" +
-            "ing>\r\n  <string>ForestGreen</string>\r\n  <string>Fuchsia</string>\r\n  <string>Gain" +
-            "sboro</string>\r\n  <string>GhostWhite</string>\r\n  <string>Gold</string>\r\n  <strin" +
-            "g>Goldenrod</string>\r\n  <string>Gray</string>\r\n  <string>Green</string>\r\n  <stri" +
-            "ng>GreenYellow</string>\r\n  <string>Honeydew</string>\r\n  <string>HotPink</string>" +
-            "\r\n  <string>IndianRed</string>\r\n  <string>Indigo</string>\r\n  <string>Ivory</stri" +
-            "ng>\r\n  <string>Khaki</string>\r\n  <string>Lavender</string>\r\n  <string>LavenderBl" +
-            "ush</string>\r\n  <string>LawnGreen</string>\r\n  <string>LemonChiffon</string>\r\n  <" +
-            "string>LightBlue</string>\r\n  <string>LightCoral</string>\r\n  <string>LightCyan</s" +
-            "tring>\r\n  <string>LightGoldenrodYellow</string>\r\n  <string>LightGray</string>\r\n " +
-            " <string>LightGreen</string>\r\n  <string>LightPink</string>\r\n  <string>LightSalmo" +
-            "n</string>\r\n  <string>LightSeaGreen</string>\r\n  <string>LightSkyBlue</string>\r\n " +
-            " <string>LightSlateGray</string>\r\n  <string>LightSteelBlue</string>\r\n  <string>L" +
-            "ightYellow</string>\r\n  <string>Lime</string>\r\n  <string>LimeGreen</string>\r\n  <s" +
-            "tring>Linen</string>\r\n  <string>Magenta</string>\r\n  <string>Maroon</string>\r\n  <" +
-            "string>MediumAquamarine</string>\r\n  <string>MediumBlue</string>\r\n  <string>Mediu" +
-            "mOrchid</string>\r\n  <string>MediumPurple</string>\r\n  <string>MediumSeaGreen</str" +
-            "ing>\r\n  <string>MediumSlateBlue</string>\r\n  <string>MediumSpringGreen</string>\r\n" +
-            "  <string>MediumTurquoise</string>\r\n  <string>MediumVioletRed</string>\r\n  <strin" +
-            "g>MidnightBlue</string>\r\n  <string>MintCream</string>\r\n  <string>MistyRose</stri" +
-            "ng>\r\n  <string>Moccasin</string>\r\n  <string>NavajoWhite</string>\r\n  <string>Navy" +
-            "</string>\r\n  <string>OldLace</string>\r\n  <string>Olive</string>\r\n  <string>Olive" +
-            "Drab</string>\r\n  <string>OrangeRed</string>\r\n  <string>Orchid</string>\r\n  <strin" +
-            "g>PaleGoldenrod</string>\r\n  <string>PaleGreen</string>\r\n  <string>PaleTurquoise<" +
-            "/string>\r\n  <string>PaleVioletRed</string>\r\n  <string>PapayaWhip</string>\r\n  <st" +
-            "ring>PeachPuff</string>\r\n  <string>Peru</string>\r\n  <string>Pink</string>\r\n  <st" +
-            "ring>Plum</string>\r\n  <string>PowderBlue</string>\r\n  <string>RosyBrown</string>\r" +
-            "\n  <string>RoyalBlue</string>\r\n  <string>SaddleBrown</string>\r\n  <string>Salmon<" +
-            "/string>\r\n  <string>SandyBrown</string>\r\n  <string>SeaGreen</string>\r\n  <string>" +
-            "SeaShell</string>\r\n  <string>Sienna</string>\r\n  <string>Silver</string>\r\n  <stri" +
-            "ng>SkyBlue</string>\r\n  <string>SlateBlue</string>\r\n  <string>SlateGray</string>\r" +
-            "\n  <string>Snow</string>\r\n  <string>SpringGreen</string>\r\n  <string>SteelBlue</s" +
-            "tring>\r\n  <string>Tan</string>\r\n  <string>Teal</string>\r\n  <string>Thistle</stri" +
-            "ng>\r\n  <string>Tomato</string>\r\n  <string>Turquoise</string>\r\n  <string>Violet</" +
-            "string>\r\n  <string>Wheat</string>\r\n  <string>WhiteSmoke</string>\r\n  <string>Yell" +
-            "ow</string>\r\n  <string>YellowGreen</string>\r\n</ArrayOfString>")]
+            "ring>DarkGreen</string>\r\n  <string>Purple</string>\r\n  <string>Aqua</string>\r\n  <" +
+            "string>Bisque</string>\r\n  <string>BlanchedAlmond</string>\r\n  <string>BlueViolet<" +
+            "/string>\r\n  <string>Brown</string>\r\n  <string>BurlyWood</string>\r\n  <string>Cade" +
+            "tBlue</string>\r\n  <string>Chartreuse</string>\r\n  <string>Chocolate</string>\r\n  <" +
+            "string>Coral</string>\r\n  <string>CornflowerBlue</string>\r\n  <string>Cornsilk</st" +
+            "ring>\r\n  <string>Crimson</string>\r\n  <string>Cyan</string>\r\n  <string>DarkBlue</" +
+            "string>\r\n  <string>DarkCyan</string>\r\n  <string>DarkGoldenrod</string>\r\n  <strin" +
+            "g>DarkGray</string>\r\n  <string>DarkKhaki</string>\r\n  <string>DarkMagenta</string" +
+            ">\r\n  <string>AntiqueWhite</string>\r\n  <string>DarkOliveGreen</string>\r\n  <string" +
+            ">DarkOrange</string>\r\n  <string>DarkOrchid</string>\r\n  <string>DarkRed</string>\r" +
+            "\n  <string>DarkSalmon</string>\r\n  <string>DarkSeaGreen</string>\r\n  <string>DarkS" +
+            "lateBlue</string>\r\n  <string>DarkSlateGray</string>\r\n  <string>DarkTurquoise</st" +
+            "ring>\r\n  <string>DarkViolet</string>\r\n  <string>DeepPink</string>\r\n  <string>Dee" +
+            "pSkyBlue</string>\r\n  <string>DimGray</string>\r\n  <string>DodgerBlue</string>\r\n  " +
+            "<string>Firebrick</string>\r\n  <string>FloralWhite</string>\r\n  <string>ForestGree" +
+            "n</string>\r\n  <string>Fuchsia</string>\r\n  <string>Gainsboro</string>\r\n  <string>" +
+            "GhostWhite</string>\r\n  <string>Gold</string>\r\n  <string>Goldenrod</string>\r\n  <s" +
+            "tring>Gray</string>\r\n  <string>Green</string>\r\n  <string>GreenYellow</string>\r\n " +
+            " <string>Honeydew</string>\r\n  <string>HotPink</string>\r\n  <string>IndianRed</str" +
+            "ing>\r\n  <string>Indigo</string>\r\n  <string>Ivory</string>\r\n  <string>Khaki</stri" +
+            "ng>\r\n  <string>Lavender</string>\r\n  <string>LavenderBlush</string>\r\n  <string>La" +
+            "wnGreen</string>\r\n  <string>LemonChiffon</string>\r\n  <string>LightBlue</string>\r" +
+            "\n  <string>LightCoral</string>\r\n  <string>LightCyan</string>\r\n  <string>LightGol" +
+            "denrodYellow</string>\r\n  <string>LightGray</string>\r\n  <string>LightGreen</strin" +
+            "g>\r\n  <string>LightPink</string>\r\n  <string>LightSalmon</string>\r\n  <string>Ligh" +
+            "tSeaGreen</string>\r\n  <string>LightSkyBlue</string>\r\n  <string>LightSlateGray</s" +
+            "tring>\r\n  <string>LightSteelBlue</string>\r\n  <string>LightYellow</string>\r\n  <st" +
+            "ring>Lime</string>\r\n  <string>LimeGreen</string>\r\n  <string>Linen</string>\r\n  <s" +
+            "tring>Magenta</string>\r\n  <string>Maroon</string>\r\n  <string>MediumAquamarine</s" +
+            "tring>\r\n  <string>MediumBlue</string>\r\n  <string>MediumOrchid</string>\r\n  <strin" +
+            "g>MediumPurple</string>\r\n  <string>MediumSeaGreen</string>\r\n  <string>MediumSlat" +
+            "eBlue</string>\r\n  <string>MediumSpringGreen</string>\r\n  <string>MediumTurquoise<" +
+            "/string>\r\n  <string>MediumVioletRed</string>\r\n  <string>MidnightBlue</string>\r\n " +
+            " <string>MintCream</string>\r\n  <string>MistyRose</string>\r\n  <string>Moccasin</s" +
+            "tring>\r\n  <string>NavajoWhite</string>\r\n  <string>Navy</string>\r\n  <string>OldLa" +
+            "ce</string>\r\n  <string>Olive</string>\r\n  <string>OliveDrab</string>\r\n  <string>O" +
+            "rangeRed</string>\r\n  <string>Orchid</string>\r\n  <string>PaleGoldenrod</string>\r\n" +
+            "  <string>PaleGreen</string>\r\n  <string>PaleTurquoise</string>\r\n  <string>PaleVi" +
+            "oletRed</string>\r\n  <string>PapayaWhip</string>\r\n  <string>PeachPuff</string>\r\n " +
+            " <string>Peru</string>\r\n  <string>Pink</string>\r\n  <string>Plum</string>\r\n  <str" +
+            "ing>PowderBlue</string>\r\n  <string>RosyBrown</string>\r\n  <string>RoyalBlue</stri" +
+            "ng>\r\n  <string>SaddleBrown</string>\r\n  <string>Salmon</string>\r\n  <string>SandyB" +
+            "rown</string>\r\n  <string>SeaGreen</string>\r\n  <string>SeaShell</string>\r\n  <stri" +
+            "ng>Sienna</string>\r\n  <string>Silver</string>\r\n  <string>SkyBlue</string>\r\n  <st" +
+            "ring>SlateBlue</string>\r\n  <string>SlateGray</string>\r\n  <string>Snow</string>\r\n" +
+            "  <string>SpringGreen</string>\r\n  <string>SteelBlue</string>\r\n  <string>Tan</str" +
+            "ing>\r\n  <string>Teal</string>\r\n  <string>Thistle</string>\r\n  <string>Tomato</str" +
+            "ing>\r\n  <string>Turquoise</string>\r\n  <string>Violet</string>\r\n  <string>Wheat</" +
+            "string>\r\n  <string>WhiteSmoke</string>\r\n  <string>Yellow</string>\r\n  <string>Yel" +
+            "lowGreen</string>\r\n</ArrayOfString>")]
         public global::System.Collections.Specialized.StringCollection SeriesColors {
             get {
                 return ((global::System.Collections.Specialized.StringCollection)(this["SeriesColors"]));
