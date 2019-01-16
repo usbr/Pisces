9180c317aff4db90e0d0c8bff5568f9260fd7a1f
Bob Lounsbury <blounsbury@usbr.gov>
2019-01-16 08:31:02 -0700

    ignore alglib tests, not mono compatible currently

--- TimeSeries/Tests/Estimation/TestUofIEstimation.cs
+++ TimeSeries/Tests/Estimation/TestUofIEstimation.cs
@@ -54,6 +54,7 @@ namespace Pisces.NunitTests.Estimation
         }
 
         [Test]
+        [Ignore("Ignore alglib tests, not mono compatible.")]
         public void UofIInterpolation()
         {
             SQLiteServer pDB = new SQLiteServer(path);
